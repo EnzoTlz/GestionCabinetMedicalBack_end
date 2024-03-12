af275d63d41ec36509179018c27f24d56465268a
EnzoTlz <enzo31del@gmail.com>
2024-03-12 19:27:43 +0100

    test CORS

--- controllers/controllerMedecin/ControllerAddMedecin.php
+++ controllers/controllerMedecin/ControllerAddMedecin.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once '../../models/Medecin.php';
 
 function checkInputToAddMedecin($data) {
