45e5aee904a8b0c1edae3a94555c12a6c42142a3
EnzoTlz <enzo31del@gmail.com>
2024-03-12 19:34:38 +0100

    test CORS

--- models/DbConfig.php
+++ models/DbConfig.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type");
 class DbConfig
 {
     private PDO $linkpdo;
