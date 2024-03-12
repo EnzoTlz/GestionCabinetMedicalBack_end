2699083a0d70980155567e8c34ed4855f0e6746b
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-03-12 10:37:26 +0100

    patch erreur getall ==> alwaysdata

--- controllers/controllerUsager/ControllerGetAllUsager.php
+++ controllers/controllerUsager/ControllerGetAllUsager.php
@@ -1,14 +1,22 @@
 <?php
-    require_once("../../models/Usager.php");
+require_once("../../models/Usager.php");
+require_once("../../models/DbConfig.php");
 
-    try {
-        $usager = new Usager();
-        $allUsager = $usager->getAllUsager();
-        
-        $usager->deliver_response(200, "Succès : La liste des usagers a été récupérée.", $allUsager);
+try {
+    // Créer une instance de DbConfig
+    $dbconfig = DbConfig::getDbConfig();
 
-    } catch (Exception $e) {
-        $usager->deliver_response(500, "Echec : La liste des usagers n'a pas été récupérée.",$e->getMessage());
-    }
+    // Créer une instance de Usager en passant l'instance de DbConfig comme argument
+    $usager = new Usager($dbconfig);
 
+    // Obtenir tous les usagers
+    $allUsager = $usager->getAllUsager();
+    
+    // Retourner une réponse réussie
+    $usager->deliver_response(200, "Succès : La liste des usagers a été récupérée.", $allUsager);
+
+} catch (Exception $e) {
+    // Retourner une réponse d'erreur avec le message d'exception
+    $usager->deliver_response(500, "Echec : La liste des usagers n'a pas été récupérée.", $e->getMessage());
+}
 ?>
