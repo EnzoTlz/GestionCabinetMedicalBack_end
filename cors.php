85e5d544b6e04fffd049077168dae0b9b7393b06
EnzoTlz <enzo31del@gmail.com>
2024-03-12 21:07:44 +0100

    ajout method PATCH dans le cors

--- cors.php
+++ cors.php
@@ -1,7 +1,7 @@
 <?php
     
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
     header("Access-Control-Allow-Headers: Content-Type");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
