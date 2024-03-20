c04a0837c10f163c7127652be63f6750f10b1819
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-03-20 10:01:46 +0100

    ajout d'un controller pour get les rdv d'un medecin par id

---
+++ controllers/controllerRendezVous/ControllerGetAllRdvByIdMedecin.php
@@ -0,0 +1,35 @@
+<?php
+    include_once '../../cors.php';
+    require_once("../../models/Rendez_vous.php");
+
+    function checkInputToGetRdv() {
+        if (!isset($_GET['id'])) {
+            $rendezVous = new Rendez_vous();
+            $rendezVous->deliver_response(400, "Echec : Id non renseignée .",null);
+            exit;
+        }
+    }
+
+    function setCommandToGetRdv($id) {
+        $rdv = new Rendez_vous();
+        $rdv->setIdRdv($id);
+
+        return $rdv;
+    }
+
+    try {
+        checkInputToGetRdv();
+        $rdv = setCommandToGetRdv($_GET['id']);
+        $rdvById = $rdv->getAllRdvMedecinByIdMedecin($_GET['id']);
+        if(!$rdvById){
+            $rdv->deliver_response(404, "Echec : Consultation non trouvé .", false);
+        }else{
+            $rdv->deliver_response(200, "Succès : Consultation bien trouvé .", $rdvById);
+        }
+
+    } catch (Exception $e) {
+        $rdv->deliver_response(500, "Echec : Consultation non trouvé .", $e->getMessage());
+    }
+
+
+?>  
