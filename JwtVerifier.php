7040d6c2ae10cef0d12f9082c87d11dd6db8a1a8
EnzoTlz <enzo31del@gmail.com>
2024-03-30 17:59:07 +0100

    modification des fonction pour les jwt

--- JwtVerifier.php
+++ JwtVerifier.php
@@ -1,32 +1,53 @@
 <?php
-// jwt_verifier.php
-function verify_jwt($jwt) {
-    $url = "https://authapigestionmedical.alwaysdata.net/authAPI/Index.php";
-    $ch = curl_init($url);
-    
-    // Préparer le cookie
-    $cookie = "usertoken=" . $jwt;
-    
-    // Set options
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_HTTPGET, true); // Utiliser une requête GET
-    curl_setopt($ch, CURLOPT_COOKIE, $cookie); // Envoyer le JWT dans un cookie
-    
-    // Execute the request
-    $result = curl_exec($ch);
 
-    // Check if any error occurred
-    if (curl_errno($ch)) {
-        $error = curl_error($ch);
-        curl_close($ch);
-        throw new Exception($error);
+    function get_authorization_header(){
+        $headers = null;
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER['Authorization']);
+        } elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+            $headers = trim($_SERVER['HTTP_AUTHORIZATION']);
+        } elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+        return $headers;
     }
 
-    $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    
-    // Vous pouvez vouloir vérifier également le contenu de $result pour des réponses plus spécifiques de l'API.
-    return $responseCode == 200;
-}
+    function get_bearer_token(){
+        $headers = get_authorization_header();
+        $retour = new Medecin();
+        if(!empty($headers)){
+            if(preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                return $matches[1];
+            }
+        }
+        $retour->deliver_response(403, "Echec : JWT non trouvé .", null);
+        return null;
+    }
+
+    function verify_jwt($jwt) {
+        $url = "https://authapigestionmedical.alwaysdata.net/authAPI/Index.php";
+        $ch = curl_init($url);
+        
+        $cookie = "usertoken=" . $jwt;
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPGET, true); 
+        curl_setopt($ch, CURLOPT_COOKIE, $cookie); 
+        
+        $result = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            throw new Exception($error);
+        }
+
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $responseCode == 200;
+    }
 ?>
--- controllers/controllerMedecin/ControllerAddMedecin.php
+++ controllers/controllerMedecin/ControllerAddMedecin.php
@@ -21,36 +21,6 @@
         return $medecin;
     }
 
-
-    function get_authorization_header(){
-        $headers = null;
-        if (isset($_SERVER['Authorization'])) {
-            $headers = trim($_SERVER['Authorization']);
-        } elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            $headers = trim($_SERVER['HTTP_AUTHORIZATION']);
-        } elseif (function_exists('apache_request_headers')) {
-            $requestHeaders = apache_request_headers();
-            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-            if (isset($requestHeaders['Authorization'])) {
-                $headers = trim($requestHeaders['Authorization']);
-            }
-        }
-        return $headers;
-    }
-    
-    function get_bearer_token(){
-        $headers = get_authorization_header();
-        $retour = new Medecin();
-        if(!empty($headers)){
-            if(preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
-                return $matches[1];
-            }
-        }
-        $retour->deliver_response(403, "Echec : JWT non trouvé .", null);
-        return null;
-    }
-    
-
     try {
         $retour = new Medecin();
 
