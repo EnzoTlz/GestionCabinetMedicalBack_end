c407cdbecc41158df2ba5c860af76714388784af
EnzoTlz <enzo31del@gmail.com>
2024-03-30 17:56:50 +0100

    changement de la requete pour GET la validé du jwt

--- JwtVerifier.php
+++ JwtVerifier.php
@@ -4,31 +4,29 @@ function verify_jwt($jwt) {
     $url = "https://authapigestionmedical.alwaysdata.net/authAPI/Index.php";
     $ch = curl_init($url);
     
+    // Préparer le cookie
+    $cookie = "usertoken=" . $jwt;
+    
     // Set options
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $jwt));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_POST, true); // Specify that you want a POST request
-    curl_setopt($ch, CURLOPT_POSTFIELDS, []); // You can set post fields if necessary
+    curl_setopt($ch, CURLOPT_HTTPGET, true); // Utiliser une requête GET
+    curl_setopt($ch, CURLOPT_COOKIE, $cookie); // Envoyer le JWT dans un cookie
     
     // Execute the request
     $result = curl_exec($ch);
-    var_dump($result);
+
     // Check if any error occurred
     if (curl_errno($ch)) {
+        $error = curl_error($ch);
         curl_close($ch);
-        throw new Exception(curl_error($ch));
+        throw new Exception($error);
     }
 
     $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
-    var_dump($$responseCode);
-    if ($responseCode == 200) {
-        // Token is valid
-        return true;
-    } else {
-        // Token is not valid
-        return false;
-    }
+    
+    // Vous pouvez vouloir vérifier également le contenu de $result pour des réponses plus spécifiques de l'API.
+    return $responseCode == 200;
 }
 ?>
