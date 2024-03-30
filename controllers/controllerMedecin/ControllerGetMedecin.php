d1ba6a6e0babfcbbfb101c5b6a2009815b742e61
EnzoTlz <enzo31del@gmail.com>
2024-03-30 18:24:10 +0100

    ajout de la verif JWT sur get medecin

--- controllers/controllerMedecin/ControllerGetMedecin.php
+++ controllers/controllerMedecin/ControllerGetMedecin.php
@@ -1,11 +1,11 @@
 <?php
     include_once '../../cors.php';
     require_once("../../models/Medecin.php");
-
+    require_once '../../JwtVerifier.php';
     function checkInputToGetMedecin() {
         $medecin = new Medecin();
         if (!isset($_GET['id'])) {
-            $medecin->deliver_response(400, "Echec : Id non renseigné.",null);
+            deliver_response(400, "Echec : Id non renseigné.",null);
             exit;
         }
     }
@@ -17,7 +17,7 @@
         $medecin->setId($idMedecin);
         $medecinExistant = $medecin->getMedecinById(); //recupere le medecin avec l'id
         if($medecinExistant === false){
-            $medecin->deliver_response(404, "Echec : Id du médecin introuvable .", $idMedecin);
+            deliver_response(404, "Echec : Id du médecin introuvable .", $idMedecin);
             return false;
         }else{
             $medecin->setId($idMedecin);
@@ -26,16 +26,22 @@
     }
 
     try {
-        checkInputToGetMedecin();
-        $medecin = setCommandToGetMedecin();
-        if ($medecin != false){
-            $medecinById = $medecin->getMedecinById();
-            $medecin->deliver_response(200, "Succès : Médecin bien trouvé .", $medecinById);
+        $jwt = get_bearer_token();
+        if(!empty($jwt)){
+            $JwtIsValid = verify_jwt($jwt);
+            if($JwtIsValid){
+                checkInputToGetMedecin();
+                $medecin = setCommandToGetMedecin();
+                if ($medecin != false){
+                    $medecinById = $medecin->getMedecinById();
+                    deliver_response(200, "Succès : Médecin bien trouvé .", $medecinById);
+                }
+            }else{
+                deliver_response(401, "Echec : Jwt non valide .", $jwt);
+            }
         }
-
-
     } catch (Exception $e) {
-        $medecin->deliver_response(500, "Echec : Médecin non trouvé .", $e->getMessage());
+        deliver_response(500, "Echec : Médecin non trouvé .", $e->getMessage());
     }
 
 
