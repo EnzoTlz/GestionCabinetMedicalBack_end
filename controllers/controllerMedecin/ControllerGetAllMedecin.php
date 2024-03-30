a8bf162c8798972f8ded4115c473f6ebc3c8ec1c
EnzoTlz <enzo31del@gmail.com>
2024-03-30 18:13:13 +0100

    ajout de la verif JWT sur getAll medecin

--- controllers/controllerMedecin/ControllerGetAllMedecin.php
+++ controllers/controllerMedecin/ControllerGetAllMedecin.php
@@ -1,13 +1,21 @@
 <?php
     include_once '../../cors.php';
     require_once("../../models/Medecin.php");
+    require_once '../../JwtVerifier.php';
 
     try {
         $medecin = new Medecin();
-        $allMedecin = $medecin->getAllMedecin();
-        
-        $medecin->deliver_response(200, "Succès : La liste des médecins a été récupérée.", $allMedecin);
 
+        $jwt = get_bearer_token();
+        if(!empty($jwt)){
+            $JwtIsValid = verify_jwt($jwt);
+            if($JwtIsValid){
+                $allMedecin = $medecin->getAllMedecin();
+                $medecin->deliver_response(200, "Succès : La liste des médecins a été récupérée.", $allMedecin);
+            }else{
+                deliver_response(401, "Echec : Jwt non valide .", $jwt);
+            }
+        }
     } catch (Exception $e) {
         $medecin->deliver_response(500, "Echec : La liste des médecins n'a pas été récupérée.",$e->getMessage());
     }
