6d0ac4413bc6aef0934b03c08c6176df12ad9c20
EnzoTlz <enzo31del@gmail.com>
2024-03-31 14:04:06 +0200

    ajout jwt get statistique

--- controllers/controllerStatistique/ControllerGetStatsMedecin.php
+++ controllers/controllerStatistique/ControllerGetStatsMedecin.php
@@ -1,13 +1,22 @@
 <?php
     include_once '../../cors.php';
     require_once '../../models/Statistique.php';
+    require_once '../../JwtVerifier.php';
 
     try{
-        $statistique = new Statistique();
-        $infoMedecin = $statistique->PrintAllNameMedecinAndAllHours();
-        $statistique->deliver_response(200, "Succès : Statistiques bien récupérées.", $infoMedecin);
+        $jwt = get_bearer_token();
+        if(!empty($jwt)){
+            $JwtIsValid = verify_jwt($jwt);
+            if($JwtIsValid){
+                $statistique = new Statistique();
+                $infoMedecin = $statistique->PrintAllNameMedecinAndAllHours();
+                deliver_response(200, "Succès : Statistiques bien récupérées.", $infoMedecin);
+            }else{
+                deliver_response(401, "Echec : Jwt non valide .", $jwt);
+            }
+        }
     }catch(Exception $e){   
-        $statistique->deliver_response(500, "Echec : Statistiques non récupérées.", $e->getMessage());
+        deliver_response(500, "Echec : Statistiques non récupérées.", $e->getMessage());
     }
 
 ?>
