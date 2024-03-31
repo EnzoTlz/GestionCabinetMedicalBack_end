a69588d6ef39195289358c0c0ca6f7a501789095
EnzoTlz <enzo31del@gmail.com>
2024-03-31 13:49:22 +0200

    ajout jwt get all Usager

--- controllers/controllerUsager/ControllerGetAllUsager.php
+++ controllers/controllerUsager/ControllerGetAllUsager.php
@@ -1,15 +1,23 @@
 <?php
     include_once '../../cors.php';
     require_once("../../models/Usager.php");
+    require_once '../../JwtVerifier.php';
 
     try {
-        $usager = new Usager();
-        $allUsager = $usager->getAllUsager();
-        
-        $usager->deliver_response(200, "Succès : La liste des usagers a été récupérée.", $allUsager);
+        $jwt = get_bearer_token();
+        if(!empty($jwt)){
+            $JwtIsValid = verify_jwt($jwt);
+            if($JwtIsValid){
+                $usager = new Usager();
+                $allUsager = $usager->getAllUsager();
+            }else{
+                deliver_response(401, "Echec : Jwt non valide .", $jwt);
+            }
+        }
+        deliver_response(200, "Succès : La liste des usagers a été récupérée.", $allUsager);
 
     } catch (Exception $e) {
-        $usager->deliver_response(500, "Echec : La liste des usagers n'a pas été récupérée.",$e->getMessage());
+        deliver_response(500, "Echec : La liste des usagers n'a pas été récupérée.",$e->getMessage());
     }
 
 ?>
