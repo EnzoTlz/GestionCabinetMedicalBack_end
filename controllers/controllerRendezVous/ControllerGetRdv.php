0e6af459ab9d7d260fae96f5dbd02cc85a8bae43
EnzoTlz <enzo31del@gmail.com>
2024-03-31 14:13:51 +0200

    ajout jwt get rdv

--- controllers/controllerRendezVous/ControllerGetRdv.php
+++ controllers/controllerRendezVous/ControllerGetRdv.php
@@ -1,11 +1,11 @@
 <?php
     include_once '../../cors.php';
     require_once("../../models/Rendez_vous.php");
+    require_once '../../JwtVerifier.php';
 
     function checkInputToGetRdv() {
         if (!isset($_GET['id'])) {
-            $rendezVous = new Rendez_vous();
-            $rendezVous->deliver_response(400, "Echec : Id non renseignée .",null);
+            deliver_response(400, "Echec : Id non renseignée .",null);
             exit;
         }
     }
@@ -18,17 +18,24 @@
     }
 
     try {
-        checkInputToGetRdv();
-        $rdv = setCommandToGetRdv($_GET['id']);
-        $rdvById = $rdv->getRdvById($_GET['id']);
-        if(!$rdvById){
-            $rdv->deliver_response(404, "Echec : Consultation non trouvé .", false);
-        }else{
-            $rdv->deliver_response(200, "Succès : Consultation bien trouvé .", $rdvById);
+        $jwt = get_bearer_token();
+        if(!empty($jwt)){
+            $JwtIsValid = verify_jwt($jwt);
+            if($JwtIsValid){
+                checkInputToGetRdv();
+                $rdv = setCommandToGetRdv($_GET['id']);
+                $rdvById = $rdv->getRdvById($_GET['id']);
+                if(!$rdvById){
+                    deliver_response(404, "Echec : Consultation non trouvé .", false);
+                }else{
+                    deliver_response(200, "Succès : Consultation bien trouvé .", $rdvById);
+                }
+            }else{
+                deliver_response(401, "Echec : Jwt non valide .", $jwt);
+            }
         }
-
     } catch (Exception $e) {
-        $rdv->deliver_response(500, "Echec : Consultation non trouvé .", $e->getMessage());
+        deliver_response(500, "Echec : Consultation non trouvé .", $e->getMessage());
     }
 
 
