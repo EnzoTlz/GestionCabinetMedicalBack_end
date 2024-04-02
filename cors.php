97187eb10fcee69094f616558323a4ee59a97eba
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 11:39:40 +0200

    modification du cors

--- cors.php
+++ cors.php
@@ -1,28 +1,10 @@
 <?php
-<?php
-// Autoriser les requêtes provenant de ce domaine
-$allowedOrigin = "https://gestionmedicalfront.alwaysdata.net";
+    header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Vérifier si l'en-tête Origin est présent dans la requête
-if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] == $allowedOrigin) {
-    // Autoriser l'accès depuis ce domaine
-    header('Access-Control-Allow-Origin: ' . $allowedOrigin);
-    // Autoriser toutes les méthodes HTTP
-    header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    // Autoriser certains autres en-têtes CORS, y compris Authorization
-    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
-    // Autoriser les cookies à être envoyés sur la demande
-    header('Access-Control-Allow-Credentials: true');
-}
-
-// Vérifier si la méthode de la requête est OPTIONS
-// Si c'est le cas, ne rien faire de plus et simplement terminer le script
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit();
-}
-
-// Traiter la requête normalement
-// Votre code pour répondre à la requête API va ici
-?>
-
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
 ?>
