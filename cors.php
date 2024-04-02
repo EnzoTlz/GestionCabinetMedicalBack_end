ba84b33b2daf7930f7fc5fff0e5b65674cac8912
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 11:52:16 +0200

    test cors acces control allow headers ==> *

--- cors.php
+++ cors.php
@@ -1,7 +1,7 @@
 <?php
     header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Headers: *");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
