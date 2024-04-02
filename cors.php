dd31cc445eae1cd1447ad4a211c0329fbf48488b
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 10:51:20 +0200

    ajout de authorization dans le cors

--- cors.php
+++ cors.php
@@ -2,7 +2,7 @@
     
     header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
