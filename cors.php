119772b38cfe01cb87c99044131331ebe9679579
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 12:01:39 +0200

    modificatin du cors test

--- cors.php
+++ cors.php
@@ -3,7 +3,9 @@
     header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Allow-Credentials: true"); // Add this line
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+    header("Access-Control-Max-Age: 3600");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
