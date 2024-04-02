7d9d2b263a089327aff11df7c4b292956672cf12
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 11:56:15 +0200

    modificatin du cors test

--- cors.php
+++ cors.php
@@ -1,9 +1,13 @@
 <?php
     header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("Access-Control-Allow-Origin: https://gestionmedical.alwaysdata.net");
+        header("Access-Control-Max-Age: 3600");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
         http_response_code(200);
         exit();
     }
